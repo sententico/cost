d545fd8445ce820cc2f3236256eb880004c3d61e
James Harvey <jjharvey@sententix.com>
2020-12-12 11:02:20 -0500

    new GoRPC wiring

--- cmon/cmon.go
+++ cmon/cmon.go
@@ -25,19 +25,12 @@ type (
 	}
 )
 
-// Test0 ...
+// GoRPC parameter/return types...
 type (
-	Test0 struct {
-		M func(string, interface{}, interface{}) error
+	Address struct {
+		Street string
+		City   string
+		St     string
+		ZIP    int
 	}
 )
-
-// Upper ...
-func (s *Test0) Upper(args string, r *string) error {
-	return s.M("Test0.Upper", args, r)
-}
-
-// Lower ...
-func (s *Test0) Lower(args string, r *string) error {
-	return s.M("Test0.Lower", args, r)
-}
